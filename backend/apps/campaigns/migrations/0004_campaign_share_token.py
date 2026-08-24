f8705403594ea6557ac7a87bfee5c44df28020d2
ajay varma <signatureglobalconferences@gmail.com>
2026-08-24 12:12:48 +0530

    fix share_token migration unique constraint

--- backend/apps/campaigns/migrations/0004_campaign_share_token.py
+++ backend/apps/campaigns/migrations/0004_campaign_share_token.py
@@ -3,6 +3,12 @@ import uuid
 from django.db import migrations, models
 
 
+def gen_uuid(apps, schema_editor):
+    MyModel = apps.get_model('campaigns', 'Campaign')
+    for row in MyModel.objects.all():
+        row.share_token = uuid.uuid4()
+        row.save(update_fields=['share_token'])
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,6 +17,12 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
+            model_name='campaign',
+            name='share_token',
+            field=models.UUIDField(default=uuid.uuid4, null=True),
+        ),
+        migrations.RunPython(gen_uuid, reverse_code=migrations.RunPython.noop),
+        migrations.AlterField(
             model_name='campaign',
             name='share_token',
             field=models.UUIDField(default=uuid.uuid4, editable=False, unique=True),
